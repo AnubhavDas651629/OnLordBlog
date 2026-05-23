2943b5d729da26dd6bd0bcf3c50970391e06090b
Anubhavdas651 <anubhavdas651@gmail.com>
2026-05-23 09:49:44 +0530

    error handling

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from re import template
 from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles 
 from fastapi.responses import HTMLResponse  # Fixed typo here (HIML -> HTML)
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
