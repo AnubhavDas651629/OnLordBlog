c73f72d37ccfd2e6507f00b51de12121de756bb2
Anubhavdas651 <anubhavdas651@gmail.com>
2026-05-23 09:04:07 +0530

    seperate post handling

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ posts: list[dict] = [
 # include_in_schema=False -> donnot add documentions in http://127.0.0.1:8000/docs
 # response_class=HTMLResponse -> helps responses get in HTML format
 
-@app.get("/", include_in_schema=False, name = "home")
+@app.get("/", include_in_schema=False , name = "home")
 @app.get("/posts", include_in_schema=False, name = "posts")
 
 # using {"posts": posts} via jinja 2 to access posts from home.html
@@ -38,6 +38,16 @@ posts: list[dict] = [
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"},)
 
-@app.get("/api/post")
+@app.get("/api/posts")
 def get_posts():  
     return posts
+
+# in this for examples an user enters api/posts/12002 in the url, then 12002 gets posted as post_id in the the function
+# anything else apart from integer returns an validation error
+app.get(f"/api/posts/{post_id}")
+def get_post(post_id: int):  
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    return{ "error": "post not found"}
+    
