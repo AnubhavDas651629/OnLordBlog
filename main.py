2f6b03efb77291bc408df5cc6c3127e0abae3f4b
Anubhavdas651 <anubhavdas651@gmail.com>
2026-05-29 12:34:20 +0530

    delete method

--- main.py
+++ main.py
@@ -229,6 +229,19 @@ def update_post_partial(
     db.commit()
     db.refresh(post)
     return post
+
+@app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalar().first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= "Post not found"
+        )
+    db.delete()
+    db.commit()
+    
     
     
 
