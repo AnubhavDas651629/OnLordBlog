59773c5e3149143d408506bae16066702d5da92c
Anubhavdas651 <anubhavdas651@gmail.com>
2026-05-29 12:06:50 +0530

    Put operations

--- main.py
+++ main.py
@@ -28,29 +28,36 @@ templates = Jinja2Templates(directory="templates")
 # include_in_schema=False -> donnot add documentions in http://127.0.0.1:8000/docs
 # response_class=HTMLResponse -> helps responses get in HTML format
 
-@app.get("/", include_in_schema=False , name = "home")
-@app.get("/posts", include_in_schema=False, name = "posts")
-
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post))
+    posts = result.scalars().all()
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"},
+    )
 # using {"posts": posts} via jinja 2 to access posts from home.html
 
 
 # request: Request -> needed to use the jijga 2 templates
-@app.get("/", include_in_schema=False, name="home")
-@app.get("/posts", include_in_schema=False, name="posts")
-def home(request: Request, db: Annotated[Session, Depends(get_db)]):
-    result= db.execute(select(models.Post))
-    posts = result.scalar().all()
-    return templates.TemplateResponse(
-        request,
-        "home.html",
-        {"posts":posts, "title":"Home"},
-    )
+# @app.get("/", include_in_schema=False, name="home")
+# @app.get("/posts", include_in_schema=False, name="posts")
+# def home(request: Request, db: Annotated[Session, Depends(get_db)]):
+#     result= db.execute(select(models.Post))
+#     posts = result.scalars().all()
+#     return templates.TemplateResponse(
+#         request,
+#         "home.html",
+#         {"posts":posts, "title":"Home"},
+#     )
 
 
 @app.get("/posts/{post_id}", include_in_schema=False)
 def post_page(request: Request,post_id: int, db: Annotated[Session, Depends(get_db)]):  
     result = db.execute(select(models.Post).where(models.Post.id == post_id))
-    post = result.scalar().first()
+    post = result.scalars().first()
     if post:
         title = post.title[:50]
         return templates.TemplateResponse(
@@ -66,7 +73,7 @@ def user_posts_page(
     user_id: int, 
     db: Annotated[Session, Depends(get_db),]
 ):  
-    result = db.execute(select(models.User).where(models.User,id == user_id))
+    result = db.execute(select(models.User).where(models.User.id == user_id))
     user = result.scalars().first()
     if not user:
         raise HTTPException(
@@ -83,7 +90,7 @@ def user_posts_page(
     )
 
 @app.post("/api/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED,)
-def create_user(user: UserResponse, db: Annotated[Session, Depends(get_db)]): #get_db each database request gets its own session and clears up after that
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]): #get_db each database request gets its own session and clears up after that
     result = db.execute(select(models.User).where(models.User.username == user.username),
     )
     existing_user = result.scalars().first()
@@ -118,13 +125,14 @@ def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(
         select(models.User).where(models.User.id == user_id),
         )
+    user = result.scalars().first()
     if user:
         return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "user not found")
 
 @app.get("/api/users/{user_id}/posts", response_model=list[PostResponse])
 def get_user_post(user_id: int, db = Annotated[Session, Depends(get_db)]):
-    result = db.excecute(select(models.User).where(models.User.id == user_id))
+    result = db.execute(select(models.User).where(models.User.id == user_id))
     user = result.scalars().first()
     if not user:
         raise HTTPException(
@@ -136,14 +144,14 @@ def get_user_post(user_id: int, db = Annotated[Session, Depends(get_db)]):
 @app.get("/api/post", response_model=list[PostResponse])
 def get_posts(db: Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.Post))
-    posts = result.scalar().all()
+    posts = result.scalars().all()
     return posts
 
 
 @app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED,)
 def create_post(post: PostCreate, db:Annotated[Session, Depends(get_db)]):
     result = db.execute(select(models.User).where(models.User.id == post.user_id))
-    user = result.scalar().first()
+    user = result.scalars().first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -163,7 +171,7 @@ def create_post(post: PostCreate, db:Annotated[Session, Depends(get_db)]):
 @app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int, db:Annotated[Session,Depends(get_db)]):
     result = db.execute(select(models.Post).where(models.Post.id == post_id))
-    post = result.scalar().first()
+    post = result.scalars().first()
     if post:
         return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not found")
@@ -185,31 +193,51 @@ def update_post_full(
         result = db.execute(
             select(models.User).where(models.User.id == post_data.user_id),
         )
-        user = result.scalar().first()
+        user = result.scalars().first()
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail = "User not found",
             )
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = post_data.user_id
 
+    db.commit()
+    db.refresh(post)
+    return post
+
+@app.patch("/api/post?{post_id}", response_model=PostResponse)
+def update_post_partial(
+    post_id: int,
+    post_data: PostUpdate,
+    db: Annotated[Session,Depends(get_db)],
+):
+    result = execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(
+            status_code= status.HTTP_404_BAD_REQUEST,
+            detail= "Post not found"
+        )
 
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
     message = (
         exception.detail
-        if exception.detail 
-        else "An error occured. Please check your request and try again"
+        if exception.detail
+        else "An error occurred. Please check your request and try again."
     )
-
     if request.url.path.startswith("/api"):
         return JSONResponse(
-            status_code= exception.status_code,
-            content = {"detail":message}
+            status_code=exception.status_code,
+            content={"detail": message},
         )
-    
-    return templates.TemplateResponse(request, "error.html",
+    return templates.TemplateResponse(
+        request,
+        "error.html",
         {
-            "status_code":exception.status_code,
+            "status_code": exception.status_code,
             "title": exception.status_code,
             "message": message,
         },
@@ -223,17 +251,17 @@ def general_http_exception_handler(request: Request, exception: StarletteHTTPExc
 def validation_exception_handler(request: Request, exception: RequestValidationError):
     if request.url.path.startswith("/api"):
         return JSONResponse(
-            status_code = status.HTTP_422_UNPROCESSABLE_CONTENT,
-            content = {"detail": exception.errors()},
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": exception.errors()},
         )
     return templates.TemplateResponse(
         request,
         "error.html",
         {
-        "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
-        "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
-        "message": "Invalid request. Please check your input and try again",
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again.",
         },
-        status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
     )
 
