11c18b03415048c0a7aa0eda74bb0e9cb1f4f68b
Anubhavdas651 <anubhavdas651@gmail.com>
2026-06-22 16:41:59 +0530

    added updates

--- image_utils.py
+++ image_utils.py
@@ -3,6 +3,7 @@ from fileinput import filename
 import uuid
 from io import BytesIO
 from PIL import Image, ImageOps
+from jinja2.bccache import Bucket
 from pydantic import FilePath
 import boto3
 from starlette.concurrency import run_in_threadpool
@@ -42,4 +43,28 @@ def process_profile_image(content: bytes) -> tuple[bytes, str]:
         output.seek(0) # moving to the begining of the file
 
     return output.read(), filename
+
+
+def _upload_to_s3(file_bytes: bytes, key:str) -> None:
+    s3 = _get_s3_client()
+    s3.upload_fileobj(
+        BytesIO(file_bytes),
+        settings.s3_bucket_name,
+        key,
+        ExtraArgs={"ContentType": "image/jpeg"},
+    )
+
+def _delete_from_s3(key:str) -> None:
+    s3 = _get_s3_client()
+    s3.delete_object(Bucket=settings.s3_bucket_name, key=key)
+
+async def upload_profile_image(file_bytes: bytes, filename:str) -> None:
+    key = f"profile_pics/{filename}"
+    await run_in_threadpool(_upload_to_s3. file_bytes, key)
+
+async def delete_profile_image(filename:str | None) -> None:
+    if filename is None:
+        return
+    key = f"profile_pics/{filename}"
+    await run_in_threadpool(_delete_from_s3, key)
 
